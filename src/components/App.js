6c80110085c042d35752fdd88a691fa04b741df7
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-02 11:53:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
+import image from '../src/image.jpeg';
 
 const App = () => {
+  const [height, setHeight] = useState(320);
+  const [width, setWidth] = useState(320);
+
+  const handleHeightChange = (event) => {
+    setHeight(parseInt(event.target.value));
+  };
+
+  const handleWidthChange = (event) => {
+    setWidth(parseInt(event.target.value));
+  };
+
   return (
     <div id="main">
-
+      <img id="resizable-img" src={image} alt="resizable" height={height} width={width} />
+      <div>
+        <label htmlFor="height-slider">Height: </label>
+        <input type="range" id="height-slider" name="height-slider" min="100" max="800" value={height} onChange={handleHeightChange} />
+      </div>
+      <div>
+        <label htmlFor="width-slider">Width: </label>
+        <input type="range" id="width-slider" name="width-slider" min="100" max="800" value={width} onChange={handleWidthChange} />
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
